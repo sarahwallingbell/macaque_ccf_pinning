08d377659e1f3fd772fc198ed6df09008b48b002
Sarah Walling-Bell <sarah.wallingbell@alleninstitute.org>
2025-03-18 14:05:22 -0700

    automate daily runs

---
+++ macaque_ccf_pinning/run_macaque_soma_pin_structures.bat
@@ -0,0 +1,4 @@
+@echo off
+call C:\Users\sarah.wallingbell\AppData\Local\anaconda3\Scripts\activate.bat morph_utils_v3
+python \\allen\programs\celltypes\workgroups\mousecelltypes\SarahWB\github_projects\macaque_ccf_pinning\macaque_ccf_pinning\macaque_soma_pin_structures.py
+call conda deactivate
